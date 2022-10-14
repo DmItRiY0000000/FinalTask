00c08bd0d5c9f198e47dec0097d44d1390881726
Dmitriy <dmit.costin2013@yandex.ru>
2022-10-14 18:58:27 +0300

    first_commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+string[] arr =                                      
+
+{
+    "bello", "2", "world", ":-)",
+    "Russia", "Denmark", "-2",  "1234", "1567",
+    "computer  science",
+};                                                  
+
+
